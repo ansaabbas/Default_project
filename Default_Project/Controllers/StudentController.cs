eb94011025a1475a0a3b3fea1bf1c43d469bd400
ansachaudhry4 <ansachaudhry4@gmail.com>
2018-09-25 00:43:11 -0700

    Delet and Edit view

--- Default_Project/Controllers/StudentController.cs
+++ Default_Project/Controllers/StudentController.cs
@@ -15,7 +15,7 @@ namespace Default_Project.Controllers
         {
             _ORM = ORM;
         }
-          
+
         [HttpGet]
         public IActionResult CreateStudent()
         {
@@ -32,13 +32,42 @@ namespace Default_Project.Controllers
 
         [HttpGet]
         public IActionResult AllStudents()
-            
+
         {
             IList<Student> AllStudents = _ORM.Student.ToList<Student>();
             return View(AllStudents);
         }
 
-        
-    
+        [HttpPost]
+        public IActionResult AllStudents(string SearchByName, string SearchByClass)
+        {
+
+            IList<Student> AllStudents = _ORM.Student.Where(m => m.Name.Contains(SearchByName) || m.Class.Contains(SearchByName)).ToList<Student>();
+            return View(AllStudents);
+        }
+        public IActionResult DeleteStudent(Student S)
+        {
+            // Student S =  ORM.Student.Where(a => a.Id == Id).FirstOrDefault<Student>();
+           _ORM.Student.Remove(S);
+            _ORM.SaveChanges();
+            //return View("AllStudents");
+            return RedirectToAction("AllStudents");
+        }
+
+        [HttpGet]
+        public IActionResult EditStudent(int Id)
+        {
+
+            Student S = _ORM.Student.Where(m => m.Id == Id).FirstOrDefault<Student>();
+            return View(S);
+        }
+        [HttpPost]
+        public IActionResult EditStudent(Student S)
+        {
+            _ORM.Student.Update(S);
+            _ORM.SaveChanges();
+            //Student S = ORM.Student.Where(m => m.Id == Id).FirstOrDefault<Student>();
+            return RedirectToAction("AllStudents");
+        }
     }
 }
